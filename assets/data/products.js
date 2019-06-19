bccd4e46cbc1ae0a886ef07342d63cbf88d99ed4
jbonet07 <jaume.bonet07@gmail.com>
2019-06-20 01:37:14 +0200

    Update 2019-06-19T23:37:11.610Z

---
+++ assets/data/products.js
@@ -0,0 +1,25 @@
+var products = [
+  {
+    category: "technology",
+    subcategory: "mobile",
+    name: "iphone",
+    image: "url",
+    description: "Best Product ever",
+    price: "800",
+    minunits: "10",
+    packs: "pc",
+    brand: "Apple"
+  },
+  {
+    category: "technology",
+    subcategory: "pc",
+    name: "hp cool pc",
+    image: "url",
+    description: "Best PC ever",
+    price: "500",
+    minunits: "15",
+    packs: "pc",
+    brand: "HP"
+  }
+];
+export default products;
